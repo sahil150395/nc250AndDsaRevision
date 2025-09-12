88f99810f110f5597c12be7121c6314981d5d43a
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-11 23:48:48 -0400

    DSA03 - NC250 - GroupAnagrams

---
+++ src/Nc250/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package Nc250;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"act","pots","tops","cat","stop","hat"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> groupedStringsMap = new HashMap<>();
+
+        for(String str : strs) {
+            char[] wordArray = str.toCharArray();
+            Arrays.sort(wordArray);
+            String sortedString = new String(wordArray);
+            List<String> stringList = groupedStringsMap.getOrDefault(sortedString, new ArrayList<>());
+            stringList.add(str);
+            groupedStringsMap.put(sortedString, stringList);
+        }
+
+        List<List<String>> groupAnagramsList = new ArrayList<>();
+        for(Map.Entry<String, List<String>> entry : groupedStringsMap.entrySet()) {
+            groupAnagramsList.add(entry.getValue());
+        }
+
+        return groupAnagramsList;
+    }
+}
