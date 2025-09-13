6d4ef162e9c642df699fc900ae21e0f53d0cf8a5
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-13 13:50:40 -0400

    DSA04 - NC250 - MajorityElement

---
+++ src/Nc250/MajorityElement.java
@@ -0,0 +1,27 @@
+package Nc250;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{5, 5, 1, 1, 1, 5, 5}));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int res = 0, maxCount = 0;
+
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+
+            if (frequency.get(num) > maxCount) {
+                res = num;
+                maxCount = frequency.get(num);
+            }
+        }
+
+        return res;
+    }
+}
