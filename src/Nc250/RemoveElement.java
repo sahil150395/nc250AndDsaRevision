e7b460433bb42f21cfb489fe15aadc121a5bb954
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-13 09:14:21 -0400

    DSA04 - NC250 - RemoveElement

--- src/Nc250/RemoveElement.java
+++ src/Nc250/RemoveElement.java
@@ -3,13 +3,13 @@ package Nc250;
 public class RemoveElement {
 
     public static int removeInPlace(int[] nums, int element) {
-        int k = 0;
+        int pointer = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != element) {
-                nums[k++] = nums[i];
+                nums[pointer++] = nums[i];
             }
         }
-        return k;
+        return pointer;
     }
 }
