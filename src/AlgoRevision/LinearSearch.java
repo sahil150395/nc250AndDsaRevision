72138e94ca6cc1691ee91612bd13e56b70375756
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-13 22:08:14 -0400

    DSA05 - Algo Revision - LinearSearch

--- src/AlgoRevision/LinearSearch.java
+++ src/AlgoRevision/LinearSearch.java
@@ -3,7 +3,7 @@ package AlgoRevision;
 public class LinearSearch {
 
     public static void main(String[] args) {
-        System.out.println(linearSearch(new int[]{1,2,3,4,5,6,7,8,9,10}, 7));
+        System.out.println(linearSearch(new int[]{1,2,3,4,5,6,7,8,9,10}, 9));
     }
 
     public static int linearSearch(int[] arr, int key) {
