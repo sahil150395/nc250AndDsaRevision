289f325fcbead3c54ab159a304fff17d148c966e
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-13 23:23:29 -0400

    DSA05 - Algo Revision - RecursiveLinearSearch

--- src/AlgoRevision/LinearSearch.java
+++ src/AlgoRevision/LinearSearch.java
@@ -3,7 +3,7 @@ package AlgoRevision;
 public class LinearSearch {
 
     public static void main(String[] args) {
-        System.out.println(linearSearch(new int[]{1,2,3,4,5,6,7,8,9,10}, 9));
+        System.out.println(linearSearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 9));
     }
 
     public static int linearSearch(int[] arr, int key) {
@@ -14,5 +14,15 @@ public class LinearSearch {
             }
         }
         return -1;
+    }
+
+    public static int recursiveLinearSearch(int[] arr, int index, int key) {
+        if (index > arr.length - 1) {
+            return -1;
+        } else if (arr[index] == key) {
+            return index;
+        } else {
+            return recursiveLinearSearch(arr, index + 1, key);
+        }
     }
 }
