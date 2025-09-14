a81889155f7e718ad01c41053eff598cc51e2054
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-09-14 16:10:35 -0400

    DSA01 - NC250 - Problem

---
+++ src/Nc250/TwoSum.java
@@ -0,0 +1,23 @@
+package Nc250;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> diffMap = new HashMap<>();
+
+        for(int i=0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if(diffMap.containsKey(diff)) {
+                return new int[] {diffMap.get(diff), i};
+            }
+
+            diffMap.put(num, i);
+        }
+        return new int[] {};
+    }
+}
