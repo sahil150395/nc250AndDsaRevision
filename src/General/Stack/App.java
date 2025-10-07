227847d0e5342bde2b7e88ae3b5ee36d30e77331
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-10-07 15:18:33 -0400

    DSA29 - Stack - DS

---
+++ src/General/Stack/App.java
@@ -0,0 +1,33 @@
+package General.Stack;
+
+public class App {
+
+    public static void main(String[] args) {
+        /*Stack theStack = new Stack(3);
+        theStack.push(20);
+        theStack.push(40);
+        theStack.push(60);
+        theStack.push(80);
+
+        while (!theStack.isEmpty()) {
+            System.out.println(theStack.pop());
+        }*/
+        System.out.println(reverseString("hello"));
+    }
+
+    public static String reverseString(String str) {
+        Stack strStack = new Stack(str.length());
+
+        for (char c : str.toCharArray()) {
+            strStack.push(c);
+        }
+
+        StringBuilder reverseString = new StringBuilder();
+
+        while (!strStack.isEmpty()) {
+            reverseString.append(strStack.pop());
+        }
+
+        return reverseString.toString();
+    }
+}
