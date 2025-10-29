da730617eddd360288842a2c3c67c4332642cc3d
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-10-28 22:06:30 -0400

    DSA45 - NC250 - BS - ReverseLinkedList

---
+++ src/Nc250/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,63 @@
+package Nc250.LinkedList;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(0);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        System.out.println(reverseList(node1).getVal());
+
+    }
+
+    /*
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    public static ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode temp = current.next; // store the next node of the current so that we dont loose track of it
+            current.next = previous; // the next node of the current node should point to the previous node
+            previous = current; // once we update the next node of the current node to previous it becomes the new previous
+            current = temp; // and the next node of the old current is the new current
+        }
+
+        return previous;
+    }
+
+}
+
+class ListNode {
+    private int val;
+    ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
