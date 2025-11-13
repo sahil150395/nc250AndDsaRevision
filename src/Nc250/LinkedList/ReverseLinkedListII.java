5b4f98245044e8451d03d12c0cbcfc1c0b86a4f0
Sahil Gulati <sahilgulati@Sahils-MacBook-Air.local>
2025-11-12 22:25:46 -0500

    DSA50 - NC250 - LL - ReverseLinkedListII

--- src/Nc250/LinkedList/ReverseLinkedListII.java
+++ src/Nc250/LinkedList/ReverseLinkedListII.java
@@ -3,7 +3,17 @@ package Nc250.LinkedList;
 public class ReverseLinkedListII {
 
     public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
 
+        reverseBetween(node1, 1, 3);
     }
 
     /*
